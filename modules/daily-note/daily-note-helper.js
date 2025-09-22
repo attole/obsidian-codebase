9e38ea95b6b75174b457997b21091e4322a33edf
attole <attole.a24d3.s@gmail.com>
2025-09-22 22:10:33 +0300

    feat: added qol functionality

--- modules/daily-note/daily-note-helper.js
+++ modules/daily-note/daily-note-helper.js
@@ -11,46 +11,56 @@ class DailyNoteHelper {
 		};
 	}
 
+	// whether provided note is daily one. tags property check not done due to long time execution on mobile 
+	isDailyNote(note) {
+		return /^\d{4}-\d{2}-\d{2}$/.test(note.basename);
+	}
+
 	async getCurrentDailyNote() {
 		// TODO get this date from db, and if none - parse to get today
 		const date = window.customJS.DateExpressionParser.parse();
 		return window.customJS.NoteManager.getNotesByName(date)[0];
 	}
 
+	// get closest daily note by provided date and direction from set folder
+	getDailyNotesByFolder(path) {
+		const notes = window.customJS.FolderManager.getDirectNotes(path);
+		return notes?.filter(note => this.isDailyNote(note));
+	}
+
 	// get closest daily note by provided date and direction from whole vault
 	getClosestDailyNote(date, direction) {
 		const { _, activePath, archivePath } = this.getStructurePathes();
 
-		return this.getClosestDailyNoteByFolders(date, direction, [
+		return this.#getClosestDailyNoteByFolders(date, direction, [
 			activePath,
 			archivePath,
 		]);
 	}
 
-	// get closest daily note by provided date and direction from set pathes
-	getClosestDailyNoteByFolders(date, direction, pathes) {
-		const dailyNotes = pathes
-			.map((path) => this.getDailyNotesByFolder(path))
-			.flat();
-
+	// get closest daily note by provided date and direction from folder pathes
+	#getClosestDailyNoteByFolders(date, direction, pathes) {
+		const dailyNotes = pathes.map((path) => this.getDailyNotesByFolder(path)).flat();
 		if (dailyNotes.length === 0) return;
-		const past = !direction || direction === 'prev';
 
+		const prev = !direction || direction === 'prev';
 		date = new Date(date);
-		return dailyNotes
-			.map((note) => ({
-				note: note,
-				date: new Date(note.basename),
-			}))
-			.filter((x) => (past ? x.date < new Date(date) : x.date > date))
-			.sort((a, b) => (past ? b.date - a.date : a.date - b.date))[0]
-			?.note;
-	}
 
-	// get closest daily note by provided date and direction from set folder
-	getDailyNotesByFolder(path) {
-		return window.customJS.FolderManager.getDirectNotes(path).filter(
-			(note) => /^\d{4}-\d{2}-\d{2}$/.test(note.basename)
-		);
+		let closest = null;
+		for (const note of dailyNotes) {
+			const noteDate = new Date(note.basename)
+
+			if (prev ? noteDate < date : noteDate > date) {
+				if (
+					!closest ||
+					(prev
+						? noteDate > closest.date
+						: noteDate < closest.date)
+				) {
+					closest = { note, date: noteDate };
+				}
+			}
+		}
+		return closest?.note;
 	}
 }
