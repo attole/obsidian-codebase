ff6bd0ca7d1b6726db0f3becd65f8e1cb291f4c8
attole <attole.a24d3.s@gmail.com>
2025-10-01 06:08:11 +0300

    refactor: complete rewrite to handle extra notes sync and fix properties

--- executables/commands/try-rollover-daily-note.js
+++ executables/commands/try-rollover-daily-note.js
@@ -1,19 +1,6 @@
 module.exports = async () => {
-	const note = app.workspace.getActiveFile();
-	if (!note || !window.customJS.DailyNoteHelper.isDailyNote(note)) {
-		const error = 'Active note is not daily note';
-		console.error(error);
-		new Notice(error);
-		return;
-	}
-
-	const dnc = window.customJS.DailyNoteContent;
-	if (await dnc.isRollovered(note)) {
-		const error = 'Note is already rollovered';
-		console.error(error);
-		new Notice(error);
-		return;
-	}
-
-	await dnc.rollover(note, false);
+	// it is much easier to call this startup run, cause it perform rollover,
+	// but also sync extra notes and updates properties
+	await window.customJS.DailyNoteStartup.run();
+	window.customJS.TabManager.closeActiveTab();
 };
