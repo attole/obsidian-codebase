defdaef24c52de742922ee422eafee9a8342f509
attole <attole.a24d3.s@gmail.com>
2025-10-04 21:39:47 +0300

    fix: proper spacing, cursor position and link handle

--- modules/events/date-observer.js
+++ modules/events/date-observer.js
@@ -7,26 +7,21 @@ class DateObserver {
 		return window.customJS.createDateExpressionParserInstance();
 	}
 
+	/*
+	 * rules if event triggered by 'space' keyword:
+	 * - if inside a link - move the space outside, and leave cursor inside straight after date
+	 * - if outside a link - append a space after the cursor as usual
+	 */
 	edit(editor, event) {
 		const cursor = editor.getCursor();
 		let text = editor.getLine(cursor.line);
 
-		/* cursor auto detect right position on some content change, but on drastic full line - not
-		due to bag (i suppose??), when use settings to auto complete brackets, all auto data and cursor 
-		are set out of them, even if before replacing content, it was inside brackets. 
-		So to avoid this, use offset of cursor position */
-		const cursorOffset = text.slice(cursor.ch, cursor.ch + 3).contains(']]')
-			? 3
-			: 0;
+		const isSpaceEvent = event.code === 'Space';
+		const isInLink = text.slice(cursor.ch, cursor.ch + 3).includes(']]');
+		if (isInLink && isSpaceEvent)
+			text = text.slice(0, cursor.ch - 1) + text.slice(cursor.ch);
 
-		// if event is using inserting space, do remove it
-		if (event.code === 'Space') {
-			text =
-				text.slice(0, cursor.ch - 1 + cursorOffset) +
-				text.slice(cursor.ch + cursorOffset);
-		}
-
-		const newText = this.#tokenizer
+		let newText = this.#tokenizer
 			.tokenize(text, this.#dateExpression.TOKENIZE_DATE_EXPRESSION)
 			.map((token) =>
 				this.#dateExpression.parse({
@@ -37,20 +32,31 @@ class DateObserver {
 			)
 			.replaceAndCollect();
 
-		if (newText.length - text.length <= cursorOffset) return;
+		if (newText === text) return;
+
+		let offset = 0;
+		if (isInLink && isSpaceEvent) {
+			newText += ' ';
+			offset = 1;
+		}
 
 		editor.replaceRange(
 			newText,
 			{ line: cursor.line, ch: 0 },
 			{
 				line: cursor.line,
-				ch: text.length,
+				ch: text.length + offset,
 			}
 		);
 
+		if (isInLink) {
+			offset = -2;
+			if (isSpaceEvent) offset -= 1;
+		}
+
 		editor.setCursor({
 			line: cursor.line,
-			ch: newText.length - cursorOffset,
+			ch: newText.length + offset,
 		});
 	}
 
