b53861729238e9793ecd82527e2ac7d685bb6913
attole <attole.a24d3.s@gmail.com>
2025-10-10 03:23:46 +0300

    refactor: simplify using existing observable hanler

--- executables/mobile/link-inline.js
+++ executables/mobile/link-inline.js
@@ -6,28 +6,7 @@ class LinkInline {
 		const editor = view.editor;
 		if (!editor) return;
 
-		let text = editor.getLine(editor.getCursor().line);
-		if (text === '') return text;
+		window.customJS.LinkObserver.edit(editor);
 
-		const linkParser = window.customJS.LinkParser;
-		const newText = window.customJS
-			.createTokenizerInstance()
-			.tokenize(text, linkParser.TOKENIZE_LINKS)
-			.map((token) =>
-				linkParser.parse({
-					input: token,
-					returnEmptyMark: false,
-					isMuted: true,
-				})
-			)
-			.replaceAndCollect();
-
-		if (newText === text) return;
-
-		editor.replaceRange(
-			newText,
-			{ line: editor.getCursor().line, ch: 0 },
-			{ line: editor.getCursor().line, ch: text.length }
-		);
 	}
 }
